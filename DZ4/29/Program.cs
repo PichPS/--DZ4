15706bf17fcfd8bb6c25324fea1bbf456d65ec1e
Pavel_Pichugin <pichps@mail.ru>
2023-02-09 09:18:22 +0300

    29 - число элементов массива

---
+++ DZ4/29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Введите введите число элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+array[i] = new Random().Next(0,100);
+Console.Write($"{array[i]}, ");
+}
+
+System.Console.Write("\b\b ");
